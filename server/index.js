9828bbed0f718e631b0532d3535ac8016369f862
Santhosh-R1 <santhoshrajan817@gmail.com>
2025-12-17 17:32:25 +0530

    error

--- server/index.js
+++ server/index.js
@@ -26,10 +26,12 @@ app.get("/", (req, res) => {
   res.send("ResQ-Link API is Running...");
 });
 
-// Start Server
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 }
+
+// CRITICAL: You must export the app for Vercel
 module.exports = app;
