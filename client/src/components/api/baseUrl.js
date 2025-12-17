7309c22b9ce6bfa13fd53bc2f9432212d2733558
Santhosh-R1 <santhoshrajan817@gmail.com>
2025-12-17 17:15:03 +0530

    vercel confi

--- client/src/components/api/baseUrl.js
+++ client/src/components/api/baseUrl.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+// VITE_API_URL will be set in Vercel settings later
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  
+  baseURL: baseURL,
 });
 
 export default axiosInstance;
--- server/index.js
+++ server/index.js
@@ -27,6 +27,9 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+module.exports = app;
